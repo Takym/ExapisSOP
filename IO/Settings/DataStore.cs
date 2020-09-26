e994394e05fd3f9bc71d3c2701d31018ac8d3b9d
Takym <takym.yt0818@gmail.com>
2020-09-26 16:14:54 +0900

    DataStore.cs中間保存

---
+++ IO/Settings/DataStore.cs
@@ -0,0 +1,38 @@
+﻿/****
+ * ExapisSOP
+ * Copyright (C) 2020 Takym.
+ * 
+ * distributed under the MIT License.
+****/
+
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace ExapisSOP.IO.Settings
+{
+	/// <summary>
+	///  設定情報を保存する為のデータストアを表します。
+	/// </summary>
+	public class DataStore : IXmlSerializable
+	{
+		public void ReadXml(XmlReader reader)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void WriteXml(XmlWriter writer)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <summary>
+		///  XMLスキーマ情報を取得します。
+		/// </summary>
+		/// <returns></returns>
+		public XmlSchema GetSchema()
+		{
+			return EnvironmentSettings.LoadSchema();
+		}
+	}
+}
